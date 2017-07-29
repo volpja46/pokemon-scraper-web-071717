3dbe9779a7ef36500c188ee8f5c92f4a76411883
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-29 12:12:06 -0400

    go

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,23 +1,20 @@
 class Pokemon
-attr_accessor :id, :name, :type, :hp, :db
+  attr_accessor :id, :name, :type, :db
 
-def initialize(id:, name:, type:, hp: nil, db:)
- @id, @name, @type, @hp, @db = id, name, type, hp, db
-end
-#saves an instance of a pokemon with the correct id
-def self.save(name, type, db)
-   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
- end
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
 
- def self.find(id, db)
-   pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
-   Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
- end
-    #  finds a pokemon from the database by
-    # their id number and returns a new Pokemon object
+  def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+  end
 
-    def alter_hp(new_hp, db)
-      db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
-    end
+  def initialize(name:, type:, db:, id:, hp:nil)
+   @id, @name, @type, @db = id, name, type, db
+  end
 
+  # def alter_hp(new_hp)
+  #   db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp)
+  # end
 end
